6d238f7d2fc450c76762719e71447607f1db5cf0
Michael P Nance <michael.p.nance@gmail.com>
2020-07-08 15:15:29 -0400

    Static html routes work now

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+// const indexRouter = require('./routes/index');
 
 const app = express();
 
 // Mongoose config
 const mongoose = require('mongoose');
-const workout = require('./models/workoutModel');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/userdb', { useNewUrlParser: true, useUnifiedTopology: true });
+const Workout = require('./models/Workout');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(require('./routes/index'));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.send('index');
-});
+// Mongoose stuff
+// const mongoose = require('mongoose');
+const Workout = require('../models/Workout');
+
+// These routes serve html pages
+router.get('/', (req, res, next) => res.send('index')); // Why does send work for index and not the other routes?
+router.get('/exercise', (req, res, next) => res.sendFile(path.join(__dirname + '/../public/exercise.html')));
+router.get('/stats', (req, res, next) => res.sendFile(path.join(__dirname + '/../public/stats.html')));
+
+// My routes go here
+
+// Need to catch error and pass to next
+router.get('/api/workouts', (req, res, next) =>
+  Workout.find().then(result => res.json(result))
+);
+
+// Need to test this one once other routes are implemented
+router.post('/api/workouts', ({ body }, res, next) =>
+  Workout.insertMany(body).then(_ => res.sendStatus(200))
+);
+
 
 module.exports = router;
